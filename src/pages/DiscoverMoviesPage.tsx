a0dc1dc8b6e5eb4b0c0e9655462ab0539a8fa2d9
Monne Onrust <MONRUST@conclusion.nl>
2020-09-10 12:32:12 +0200

    disc movies

--- src/pages/DiscoverMoviesPage.tsx
+++ src/pages/DiscoverMoviesPage.tsx
@@ -1,36 +1,71 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+type SearchState =
+  | { status: "idle" }
+  | { status: "loading" }
+  | { status: "success"; data: any } // todo: specify the data type too
+  | { status: "error"; error: any };
+
 export default function DiscoverMoviesPage() {
   const [movies, setMovies] = useState([]);
-  const [searchText, setSearchText] = useState("life");
+  const [searchTextTitle, setsearchTextTitle] = useState("life");
+  const [searchTextYear, setSearchTextYear] = useState("");
+  const [searchState, setSearchState] = useState<SearchState>({
+    status: "idle",
+  });
 
   useEffect(() => {
     searchMovies();
   }, []);
 
   const searchMovies = async () => {
-    if (searchText === "") {
+    if (searchTextTitle === "") {
       setMovies([]);
       return;
     }
 
-    const res = await axios.get(
-      "http://www.omdbapi.com/?s=" + searchText + "&apikey=1bb277ac"
-    );
+    setSearchState({ status: "loading" });
 
-    setMovies(res.data.Search);
+    const newMovies = await getMovies();
+
+    setMovies(newMovies);
+
+    //setSearchState({ status: "success" });
+  };
+
+  const getMovies = async () => {
+    let url =
+      "http://www.omdbapi.com/?s=" +
+      encodeURIComponent(searchTextTitle) +
+      "&apikey=1bb277ac";
+
+    if (searchTextYear) {
+      url += "&y=" + searchTextYear;
+    }
+
+    const res = await axios.get(url);
+
+    return res.data.Search;
   };
 
   return (
     <div>
-      <span>Search</span>
-      <input
-        placeholder="Title"
-        value={searchText}
-        onChange={(e) => setSearchText(e.target.value)}
-      />
-      <button onClick={searchMovies}>Search</button>
+      <p>
+        <span>Search</span>
+        <input
+          placeholder="Title"
+          value={searchTextTitle}
+          onChange={(e) => setsearchTextTitle(e.target.value)}
+        />
+        <input
+          placeholder="Release year"
+          value={searchTextYear}
+          onChange={(e) => setSearchTextYear(e.target.value)}
+        />
+        <button onClick={searchMovies}>Search</button>
+        <span>{searchState.status}</span>
+      </p>
       <ul>
         {movies.map((m: any) => {
           return <li key={m.Title}>{m.Title}</li>;
