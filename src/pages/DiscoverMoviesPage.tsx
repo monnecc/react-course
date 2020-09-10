74591a7d2b9b5fca78adf709f5f5bac45ef278da
Monne Onrust <MONRUST@conclusion.nl>
2020-09-10 12:16:46 +0200

    routing discover movies

--- src/pages/DiscoverMoviesPage.tsx
+++ src/pages/DiscoverMoviesPage.tsx
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function DiscoverMoviesPage() {
-  return <h1>Discover movies</h1>;
+  const [movies, setMovies] = useState([]);
+  const [searchText, setSearchText] = useState("life");
+
+  useEffect(() => {
+    searchMovies();
+  }, []);
+
+  const searchMovies = async () => {
+    if (searchText === "") {
+      setMovies([]);
+      return;
+    }
+
+    const res = await axios.get(
+      "http://www.omdbapi.com/?s=" + searchText + "&apikey=1bb277ac"
+    );
+
+    setMovies(res.data.Search);
+  };
+
+  return (
+    <div>
+      <span>Search</span>
+      <input
+        placeholder="Title"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <button onClick={searchMovies}>Search</button>
+      <ul>
+        {movies.map((m: any) => {
+          return <li key={m.Title}>{m.Title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
